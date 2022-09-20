758bb00b0ffb62b69b1f5d2a9c8dfbbcfb701850
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-09-20 19:46:18 +0000

    Finished Discord OAuth

---
+++ script.js
@@ -0,0 +1,8 @@
+function onDiscordButtonClick(){
+
+	window.open("https://discord.com/api/oauth2/authorize?client_id=1021824465140727860&redirect_uri=https%3A%2F%2Fvaliantwind.me%2FOAuth-2.0-Playground%2F&response_type=code&scope=identify", "_blank")
+}
+
+function onTwitterButtonClick(){
+	
+}
